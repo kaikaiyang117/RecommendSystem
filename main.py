172c365720646844b548a145ca9ba3ce0497cf87
kky <2996210796@qq.com>
2024-12-05 13:25:00 +0800

    add test Get

--- main.py
+++ main.py
@@ -11,3 +11,16 @@ def read_item(user_agent: str = Header(None), session_token: str = Cookie(None))
 @app.get("/redirect")
 def redirect():
     return RedirectResponse(url="/items/")
+
+
+# 查询对应user的信息 user：name return json
+
+# 查询activaity信息 user：name return json
+
+# 添加用户
+
+# 添加活动
+
+# 通过csv导入neo4j数据库信息
+
+# 为推荐推荐合适信息 user：name return list[json]
